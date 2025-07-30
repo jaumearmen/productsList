c2c4ecfcb995a1a420c8a90e7e5aadba0894c0da
berges99 <daviid.bergees@gmail.com>
2025-07-30 11:58:53 +0200

    Changed favicon and made html title dynamic via env variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Leads from './pages/Leads'
 import LeadDetail from './pages/LeadDetail'
+import { useTitle } from './hooks/use-title'
 
 function App() {
+  const appTitle = import.meta.env.VITE_APP_TITLE;
+  if (appTitle) {
+    useTitle(appTitle);
+  }
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-background">
